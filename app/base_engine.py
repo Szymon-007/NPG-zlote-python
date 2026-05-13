3db052375797e2397818a161eee24187307e0805
Emil <escibich@gmail.com>
2026-05-14 00:48:50 +0200

    dodano metode latwego dostepu do bazy

--- app/base_engine.py
+++ app/base_engine.py
@@ -4,5 +4,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 base  = create_engine('sqlite:///bazalogin.db', echo=True)
 session  = sessionmaker(bind=base)
 base = declarative_base()
-
-
+def get_db():
+    bazadanych = session()
+    try:
+        yield bazadanych
+    finally:
+        bazadanych.close()
