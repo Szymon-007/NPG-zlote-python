33a6d18ccf3d270bf8dedd79a7cc1b87368a0870
Emil <escibich@gmail.com>
2026-05-14 20:07:51 +0200

    dodano tagi do sessionmaker, poprawiono skladnie i nazewnictwo

--- app/base_engine.py
+++ app/base_engine.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-base  = create_engine('sqlite:///bazalogin.db', echo=True)
-session  = sessionmaker(bind=base)
+engine  = create_engine('sqlite:///bazalogin.db', echo=True)
+session  = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 base = declarative_base()
 def get_db():
     bazadanych = session()
