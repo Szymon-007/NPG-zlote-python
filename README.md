66894052b8a97eb2b42e4946b6a2ef6f933442cf
Emil <escibich@gmail.com>
2026-05-19 11:49:20 +0200

    dodano funkcje haszowania hasla, poprawiono konfuguracje z baza od bartka

--- README.md
+++
@@ -1,2 +0,0 @@
-# NPG-zlote-mysli
-Projekt na przedmiot narzędzia pracy grupowej temat: zlota mysl na kazdy dzien
--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
-from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
-from starlette.responses import FileResponse
-
-from base_engine import get_db, base, engine
-from schemat import *
+from typing import Optional
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
-from base import User
 from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
+from sqlmodel import Field, SQLModel, Session, select, create_engine
+from passlib.context import CryptContext
+from pydantic import BaseModel
 app=FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 base.metadata.create_all(bind=engine)
@@ -17,16 +16,27 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+def hash_password(password: str) ->str:
+    return pwd_context.hash(password)
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
 
-@app.post("/")
+@app.post("/reg")
 def register(data: UserRegister ,baza:Session = Depends(get_db)):
+    statement  = select(User).where(User.email == data.email)
+    existing_User = baza.execute(statement).first()
+    if existing_User:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    haszowane= hash_password(data.password)
     nowyuz = User(name = data.name,
     email = data.email,
     password = data.password)
     baza.add(nowyuz)
     baza.commit()
     baza.refresh(nowyuz)
-
+@app.post("/log")
+def login(data:  )
 
 
 @app.get("/")
