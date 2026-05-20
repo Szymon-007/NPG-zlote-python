bf19fc5c80c203c800d4f3e16ef9ee01fa2e53d4
Emil <escibich@gmail.com>
2026-05-20 10:03:42 +0200

    oznaczono funkcjie komentarzami

--- app/main.py
+++ app/main.py
@@ -4,20 +4,17 @@ from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from sqlmodel import SQLModel, select
 from passlib.context import CryptContext
-from bas import get_db, User, engine
+
 from models import UserLogin, UserRegister
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
+#haszowanie hasla zad 3.1
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
-
-
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+
 
 app = FastAPI()
 
@@ -27,11 +24,10 @@ app = FastAPI()
 def on_startup():
     create_db_and_tables()
 
-
+#endpoint rejestracji zad 3.2
 @app.post("/reg")
 def register(data: UserRegister, baza: Session = Depends(get_db)):
-    statement = select(User).where(User.email == data.email)
-    existing_user = baza.execute(statement).scalars().first()
+
 
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
@@ -39,23 +35,10 @@ def register(data: UserRegister, baza: Session = Depends(get_db)):
     # Wygenerowanie hasha z odebranego jawnego hasła
     haszowane = hash_password(data.password)
 
-    # Inicjalizacja nowego obiektu w bazie (zostało tu naprawione przypisanie hasła)
-    nowyuz = User(
-        name=data.name,
-        email=data.email,
-        hashed_password=haszowane
-    )
-    baza.add(nowyuz)
-    baza.commit()
-    baza.refresh(nowyuz)
-
-    return {"message": "User registered successfully", "user_id": nowyuz.id}
-
-
+#endpoint logowania zad 3.3
 @app.post("/log")
 def login(data: UserLogin, baza: Session = Depends(get_db)):
-    statement = select(User).where(User.email == data.email)
-    user = baza.execute(statement).scalars().first()
+
 
     if not user or not verify_password(data.password, user.hashed_password):
         raise HTTPException(status_code=400, detail="Incorrect email or password")
@@ -63,10 +46,3 @@ def login(data: UserLogin, baza: Session = Depends(get_db)):
     return {"message": "Logged in successfully", "user_id": user.id}
 
 
-
-if __name__ == "__main__":
-    create_db_and_tables()
-    print("Baza danych i tabela User zostały pomyślnie utworzone!")
-
-    test_hash = hash_password("SuperTajneHaslo123")
-    print(f"Przykładowy hash dla Twojego hasła: {test_hash}")
