0fe764bea3e252f7572d93b845ce1f12f404961f
Emil <escibich@gmail.com>
2026-05-20 10:34:34 +0200

    #closes 12 #closes 13

--- app/main.py
+++ app/main.py
@@ -2,14 +2,19 @@
 from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.functions import user
 from sqlmodel import SQLModel, select
 from passlib.context import CryptContext
-
 from models import UserLogin, UserRegister
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from typing import Optional
+from JWTgen import create_access_token
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 #haszowanie hasla zad 3.1
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 def verify_password(plain_password: str, hashed_password: str) -> bool:
@@ -36,12 +41,15 @@ def register(data: UserRegister, baza: Session = Depends(get_db)):
     haszowane = hash_password(data.password)
 
 #endpoint logowania zad 3.3
+#przydiela acces token
 @app.post("/log")
 def login(data: UserLogin, baza: Session = Depends(get_db)):
 
-
     if not user or not verify_password(data.password, user.hashed_password):
         raise HTTPException(status_code=400, detail="Incorrect email or password")
+    dane = {"sub": str(user.id), "email": str(user.email)}
+    token = create_access_token(data = dane)
+    return {"access_token": token, "token_type": "bearer"}
 
     return {"message": "Logged in successfully", "user_id": user.id}
 
