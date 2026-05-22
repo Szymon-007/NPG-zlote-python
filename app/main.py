3d96599eb9b597a5fdc0971b3fdeca55c0b5ee0b
Emil <escibich@gmail.com>
2026-05-22 21:14:01 +0200

    przetestowano i ujednolicono sciezki do plikow

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlmodel import Session
 import passlib.context
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.models_mimics import UserLogin, UserRegister
 from app.JWTgen import create_access_token, get_current_user
-from app.database import pobierz_sesje
+from baza_danych.database import pobierz_sesje, stworz_tabele
 
 # Importujemy funkcje z Twojego pliku crud
-from app.crud import pobierz_uzytkownika_po_mailu, dodaj_uzytkownika
+from baza_danych.crud import pobierz_uzytkownika_po_mailu, dodaj_uzytkownika
 
 # Haszowanie hasla zad 3.1
 pwd_context = passlib.context.CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -22,7 +22,16 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"], # Porty Twojego Reacta
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.on_event("startup")
+def on_startup():
+    stworz_tabele()
 
 # Endpoint rejestracji zad 3.2
 # Zmieniona ścieżka na /api/register
@@ -34,20 +43,21 @@ def register(data: UserRegister, baza: Session = Depends(pobierz_sesje)):
         raise HTTPException(status_code=400, detail="Email already registered")
 
     haszowane = hash_password(data.password)
-    nowy_user = dodaj_uzytkownika(baza, data.email, haszowane, data.zodiac)
 
-    # Dodano generowanie tokena po rejestracji (zgodnie z instrukcją z README)
+    # TUTAJ ZMIANA: z data.zodiac na data.zodiac_sign
+    nowy_user = dodaj_uzytkownika(baza, data.email, haszowane, data.zodiac_sign)
+
     dane = {"sub": str(nowy_user.id), "email": str(nowy_user.email)}
     token = create_access_token(data=dane)
 
     return {
         "message": "Registered successfully",
-        "access_token": token,  # <--- Frontend zapisze to w localStorage
+        "access_token": token,
         "token_type": "bearer"
     }
 
 
-# Logowanie zostaje takie, jakie było (tylko upewnij się, że jest /api/login)
+
 @app.post("/api/login")
 def login(data: UserLogin, baza: Session = Depends(pobierz_sesje)):
     user = pobierz_uzytkownika_po_mailu(baza, data.email)
@@ -64,5 +74,4 @@ def login(data: UserLogin, baza: Session = Depends(pobierz_sesje)):
 # Endpoint statusu
 @app.get("/status-uzytkownika")
 def sprawdz_status_ankiety(current_user=Depends(get_current_user), baza: Session = Depends(pobierz_sesje)):
-    # Zmieniony klucz JSON-a zgodnie z zaleceniem frontendu
     return {"has_filled_today": False}
